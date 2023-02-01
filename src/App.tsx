75a9008cf6479d7f5b816bb1045a79f674443137
mert.alptekin <mert.alptekin@neomina.com>
2023-02-01 14:24:32 +0300

    AbortedController konusu ve CleanUp Functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,116 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { ITodo, ITodoUser } from "./models/todo";
 import Todo from "./components/Todo";
+import * as _ from "lodash";
 
 function App() {
-  //
+  const controller = new AbortController();
+  // Then Catch Promise yapısı kullanımı
+  const todoPromise = () =>
+    new Promise((resolve, reject) => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => {
+          let todoList = response.data as any[];
 
-  const todoPromise = new Promise((resolve, reject) => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => {
-        let todoList = response.data as any[];
+          console.log("response", todoList);
 
-        console.log("response", todoList);
+          // her bir todo item değeri için apidan istek atarak user bilgisine erişmem gerektiğinde dolayı promise then yöntemi ile birleştiremedik. // mantıken promise.all ile de yapamadık (farklı promiseler tek bir sonuç döndürsün diye var)
+          todoList.forEach((item: ITodo) => {
+            // async bir istek var.
+            axios
+              .get(`https://jsonplaceholder.typicode.com/users/${item.userId}`)
+              .then((response: any) => {
+                let user = Object.assign({
+                  id: response.data.id,
+                  name: response.data.name,
+                  username: response.data.username,
+                });
 
-        // her bir todo item değeri için apidan istek atarak user bilgisine erişmem gerektiğinde dolayı promise then yöntemi ile birleştiremedik. // mantıken promise.all ile de yapamadık (farklı promiseler tek bir sonuç döndürsün diye var)
-        todoList.forEach((item: ITodo) => {
-          // async bir istek var.
-          axios
-            .get(`https://jsonplaceholder.typicode.com/users/${item.userId}`)
-            .then((response: any) => {
-              let user = Object.assign({
-                id: response.data.id,
-                name: response.data.name,
-                username: response.data.username,
+                item.user = { ...(user as ITodoUser) }; // spread operatörü ile user nesneni güncelleyecek.
               });
+          });
 
-              item.user = { ...(user as ITodoUser) }; // spread operatörü ile user nesneni güncelleyecek.
-            });
-        });
+          resolve(response.data);
+        })
+        .catch((err) => reject(err));
+    });
 
-        resolve(response.data);
+  let todos: any[] = []; // variable (Bunu angular yapar, Gerçek DOM var, ChangeTracker diye bir mekanizma variable değişikliğini direk doma yansıtıyor.)
+
+  const [todoState, setTodoState] = useState<ITodo[]>([]);
+
+  const fetchTodo = async (signal: AbortSignal) => {
+    let todoRes = (await (
+      await axios.get("https://jsonplaceholder.typicode.com/todos", {
+        signal: signal,
       })
-      .catch((err) => reject(err));
-  });
+    ).data) as ITodo[];
+
+    let todoRes2: any[] = [];
+
+    // loadash ile farklı referanslarda copy işlemleri yapabiliriz.
+    todoRes2 = _.cloneDeep(todoRes); // array ve object içinde kullanılabilir.
+
+    todoRes.forEach(async (item: ITodo) => {
+      // yada referance durumu yaratan object,array (object) gibi js yapılarını spread operatörü yardımı ile eşlemek referans hatalarının çnüne geçer. Object.assing() yeni ref tanımı = {... item} [... items]
+      item.user = {
+        ...((
+          await axios.get(
+            `https://jsonplaceholder.typicode.com/users/${item.userId}`,
+            { signal: signal }
+          )
+        ).data as ITodoUser),
+      };
+
+      // console.log("item.user", item.user);
+    });
+
+    todos = [...todoRes];
+    setTodoState(todoRes); // yukarıdaki variable yerine state güncellenir.
+
+    // Not: sayfa açıldığında sayfada render edilecek olan değerleri useEffect ile apiden çekiririz. bu apiden gelen verilerin sayfaya yansımasını istiyorsak bu durumda useState kullanmamız gerekir. Aksi takdirde veri çekilecek ama component re-render olmayacağından arayüze yansımayacaktır. (Virtual DOM değişikliği sadece state güncellemesi ile oluyor.)
+    console.log("todos", todos);
+  };
 
   useEffect(() => {
-    console.log("promise");
-    todoPromise
-      .then((todoResponse) => {
-        console.log("todoResponse", todoResponse); // {id,title,completed,user:{id,name}} beklenen result<
-      })
-      .catch((err) => {
-        console.log("err", err);
-      });
+    // async her bir her bir api call işleminde kullanmayı tavsiye ederiz.
+    // api kaynak tüketimini iş bittikten sonra kesme görevi görüyor
+    fetchTodo(controller.signal);
+
+    // clean up function
+    return () => {
+      console.log("clean-up");
+      controller.abort(); // artık bu component ile işim bitti http istediğini sonlandır. diğer componentler etkilenmesin.
+    };
+  }, []);
+
+  useEffect(() => {
+    // console.log("promise");
+    // todoPromise()
+    //   .then((todoResponse) => {
+    //     console.log("todoResponse", todoResponse); // {id,title,completed,user:{id,name}} beklenen result<
+    //   })
+    //   .catch((err) => {
+    //     console.log("err", err);
+    //   });
 
     return () => {};
   }, []); // tek sefer sayfa açıldığında çalışsın
 
-  // const res = () => {
-  //   // this // windows
-  // }
-
-  // function res2() {
-  //   // this self kendi context
-  // }
-
-  // res(){}
+  const controllerAbort = () => {
+    controller.abort();
+    console.log("self clean up");
+  };
 
   return (
     <div className="App">
-      <Todo items={[]} />
+      <button onClick={controllerAbort}>Custom Abort</button>
+      <button onClick={() => setTodoState([])}>State Temizle</button>
+      <span>{todoState.length > 0 && <Todo items={todoState} />}</span>
     </div>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,6 @@
 // tfc
 
-import React from "react";
+import React, { memo, useEffect, useMemo } from "react";
 import { ITodo } from "../models/todo";
 
 type TodoProps = {
@@ -9,16 +9,30 @@ type TodoProps = {
 // map ile diğer backend dillerinde olduğu gibi arayüze gelen bir nesnenin iterate edilecek şekilde yazılmasını sağlarız. (foreach, for)
 export const Todo = (
   { items }: TodoProps // parent component
-) => (
-  <>
-    <h1>TodoList</h1>
-    <ul>
-      {items.map((item: ITodo) => {
-        return <TodoItem todo={item} />; // burdaki template değerini return et
-      })}
-    </ul>
-  </> // tek bir jsx return ediyor
-);
+) => {
+  // let interval1 = setInterval(() => {
+  //   console.log("deneme");
+  // }, 100);
+
+  useEffect(() => {
+    return () => {
+      console.log("dom destroy");
+      // clearInterval(interval1);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>TodoList</h1>
+      <ul>
+        {items.map((item: ITodo) => {
+          return <TodoItem key={item.id} todo={item} />; // burdaki template değerini return et
+          // diziler ile çalışırken index kullanmayalım, sıramala işlemlerinde domadaki bir çok node gereksiz re-rendera sebebiyet verir.
+        })}
+      </ul>
+    </>
+  ); // tek bir jsx return ediyor)
+};
 
 export default Todo;
 
@@ -26,7 +40,7 @@ type TodoItemProps = {
   todo: ITodo; // {id:1,completed:false,name:'ali',title:'deneme'}
 };
 // her bir item değerini ekrana yansıtacak.
-export const TodoItem = ({ todo }: TodoItemProps) => {
+export const TodoItem = memo(({ todo }: TodoItemProps) => {
   // child component
   return (
     <>
@@ -38,4 +52,4 @@ export const TodoItem = ({ todo }: TodoItemProps) => {
       </li>
     </>
   );
-};
+});
