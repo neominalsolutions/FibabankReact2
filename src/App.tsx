11b855e5fac1a4c20f87d7700778cdd126255e1c
mert.alptekin <mert.alptekin@neomina.com>
2023-02-01 11:57:15 +0300

    TodoPromise yapısı

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { ITodo, ITodoUser } from "./models/todo";
+import Todo from "./components/Todo";
 
 function App() {
+  //
+
+  const todoPromise = new Promise((resolve, reject) => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        let todoList = response.data as any[];
+
+        console.log("response", todoList);
+
+        // her bir todo item değeri için apidan istek atarak user bilgisine erişmem gerektiğinde dolayı promise then yöntemi ile birleştiremedik. // mantıken promise.all ile de yapamadık (farklı promiseler tek bir sonuç döndürsün diye var)
+        todoList.forEach((item: ITodo) => {
+          // async bir istek var.
+          axios
+            .get(`https://jsonplaceholder.typicode.com/users/${item.userId}`)
+            .then((response: any) => {
+              let user = Object.assign({
+                id: response.data.id,
+                name: response.data.name,
+                username: response.data.username,
+              });
+
+              item.user = { ...(user as ITodoUser) }; // spread operatörü ile user nesneni güncelleyecek.
+            });
+        });
+
+        resolve(response.data);
+      })
+      .catch((err) => reject(err));
+  });
+
+  useEffect(() => {
+    console.log("promise");
+    todoPromise
+      .then((todoResponse) => {
+        console.log("todoResponse", todoResponse); // {id,title,completed,user:{id,name}} beklenen result<
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+
+    return () => {};
+  }, []); // tek sefer sayfa açıldığında çalışsın
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo items={[]} />
     </div>
   );
 }
